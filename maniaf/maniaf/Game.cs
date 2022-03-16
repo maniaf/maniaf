5817df03b88e84fbaa71e868fb951f2eb9e320b0
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-03-17 02:42:46 +0900

    225

---
+++ maniaf/maniaf/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using SixLabors.ImageSharp.PixelFormats;
+using Image = SixLabors.ImageSharp.Image;
+using SixLabors.ImageSharp;
+using System.Runtime.InteropServices;
+
+namespace maniaf
+{
+    internal class maniafgame : GameWindow
+    {
+        public maniafgame()
+            : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.CenterWindow(new Vector2i(1600, 900));
+            Image<Rgba32> image = (Image<Rgba32>)Image.Load(Configuration.Default, "../../../assets/maniaf225.png");
+            image.TryGetSinglePixelSpan(out var span);
+            byte[] pixels = MemoryMarshal.AsBytes(span).ToArray();
+            this.Icon = new OpenTK.Windowing.Common.Input.WindowIcon(new OpenTK.Windowing.Common.Input.Image(225, 225, pixels));
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(new Color4(0f, 0f, 0f, 1f));
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            this.Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+    }
+}
---
+++ maniaf/maniaf/Program.cs
@@ -0,0 +1,15 @@
+﻿using maniaf;
+
+namespace Maniaf
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (maniafgame game = new maniafgame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
